276556a786cbb8d01b76e739afa70faae0a1838b
paulni <paulni@bentonvillek12.org>
2020-07-03 19:30:45 -0500

    Initial Commit

---
+++ src/main/java/com/application/financenow/FinanceNowApplication.java
@@ -0,0 +1,13 @@
+package com.application.financenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceNowApplication.class, args);
+    }
+
+}
