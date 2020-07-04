ab2095c3be2448f07b50264faea6884d182d0917
paulni <paulni@bentonvillek12.org>
2020-07-04 18:04:45 -0500

    More Setup Commit

--- src/main/java/com/application/financenow/controllers/BillsController.java
+++ src/main/java/com/application/financenow/controllers/BillsController.java
@@ -1,18 +1,25 @@
 package com.application.financenow.controllers;
 
+import com.application.financenow.models.data.BillsDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("bills")
 public class BillsController {
 
-    @RequestMapping("")
-    @ResponseBody
-    public String display(Model model){
+    @Autowired
+    private BillsDao billsDao;
 
-        return "Hello World";
+    @GetMapping
+    public String display(Model model){
+        model.addAttribute("bills", billsDao.findAll());
+        model.addAttribute("mainTitle", "List of Bills");
+
+
+        return "display";
     }
 }
